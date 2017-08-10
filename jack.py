7e7dea2b5f5fb10c691b1b7e23232998c49335b0
DESKTOP-AT6VITV\Wade Zengel <wadezengel@gmail.com>
2017-08-10 12:56:13 -0400

    Removed channel refrences that do not exist in opening message.

--- jack.py
+++ jack.py
@@ -18,7 +18,7 @@ async def on_message(message):
 @client.event
 async def on_member_join(member):
     server = member.server
-    msg = 'Welcome to the Tekken 7 beginners discord {0.mention} ! You have been given the NewBlood role to help the bot assign your other roles. Make sure to check out the #set_role channel to choose your roles! You can use HELP_COMMAND to see a list of commands or check out #bot_commands.'
+    msg = 'Welcome to the Tekken 7 beginners discord {0.mention} ! You have been given the NewBlood role to help the bot assign your other roles. Make sure to check out the set_role channel to choose your roles! You can use HELP_COMMAND to see a list of commands or check out bot_commands.'
     await client.send_message(server, msg.format(member))
 
 fh = open('jack.conf', 'r')
