06536aae47b40550506324d6b7ef149cae8589d4
DESKTOP-AT6VITV\Wade Zengel <wadezengel@gmail.com>
2017-08-10 13:31:57 -0400

    Removed the format function from he message.

--- jack.py
+++ jack.py
@@ -19,7 +19,7 @@ async def on_message(message):
 async def on_member_join(member):
     myServer = member.server
     msg = 'Welcome to the Tekken 7 beginners discord {0.mention} ! You have been given the NewBlood role to help the bot assign your other roles. Make sure to check out the set_role channel to choose your roles! You can use HELP_COMMAND to see a list of commands or check out bot_commands.'
-    await client.send_message(344343848329150465, msg.format(member))
+    await client.send_message(myServer.default_channel, msg)
 
 fh = open('jack.conf', 'r')
 jack_key = fh.readline().rstrip()
