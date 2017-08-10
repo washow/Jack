683d900a3b3f9296730f175dd879175f3691b8ad
DESKTOP-AT6VITV\Wade Zengel <wadezengel@gmail.com>
2017-08-10 12:40:30 -0400

    Added first draft of greeting message similar to current echo greeting.

--- jack.py
+++ jack.py
@@ -15,6 +15,11 @@ async def on_message(message):
     if message.content.startswith('!ping'):
        await client.send_message(message.channel, 'Pong!')
 
+@client.event
+async def on_member_join(member):
+    msg = 'Welcome to the Tekken 7 beginners discord {0.mention} ! You have been given the NewBlood role to help the bot assign your other roles. Make sure to check out the #set_role channel to choose your roles! You can use HELP_COMMAND to see a list of commands or check out #bot_commands.'
+    await client.send_message(server, msg.format(member))
+
 fh = open('jack.conf', 'r')
 jack_key = fh.readline().rstrip()
 client.run(jack_key)
